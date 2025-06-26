1d93aefabd2d1f4407f06664f0001fb4fcd279f1
Hamilton Oliveira <jhamilton.oliveira@gmail.com>
2025-06-25 22:31:13 -0300

    Ajustando layout do sign_up

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,6 @@
 class Users::RegistrationsController < Devise::RegistrationsController
   layout 'devise'
   
-  # Skip authenticate_user! for registration actions
-  skip_before_action :authenticate_user!, except: [:show, :edit, :update, :destroy]
+  # Skip authenticate_user! for registration actions (new, create)
+  skip_before_action :authenticate_user!, only: [:new, :create]
 end
