a30cdd19f93246c92c88900d76b6b9418bfe1072
Hamilton Oliveira <jhamilton.oliveira@gmail.com>
2025-06-30 11:42:13 -0300

    Alterando Action

--- app/controllers/users/passwords_controller.rb
+++ app/controllers/users/passwords_controller.rb
@@ -2,5 +2,5 @@ class Users::PasswordsController < Devise::PasswordsController
   layout "devise"
 
   # Skip authenticate_user! for password reset
-  skip_before_action :authenticate_user!
+  # skip_before_action :authenticate_user!
 end
